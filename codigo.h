f0f20b7c727d2e797cae651d60597084d0e31b88
elpai <rodripait@gmail.com>
2016-07-02 17:48:45 -0300

    actualizacion

---
+++ codigo.h
@@ -0,0 +1,14 @@
+#ifndef CODIGO_H
+#define CODIGO_H  1
+
+
+#define OP_PACK            (1)
+#define OP_RPACK           (2)
+#define OP_UNPACK          (3)
+#define OP_UNPACKALL       (4)
+#define OP_LIST            (5)
+#define OP_DETAIL          (6)
+#define OP_HELP       	   (0)
+
+
+#endif
--- funciones_empack.h
+++ funciones_empack.h
@@ -1,8 +1,8 @@
 #ifndef FUN_EMP
 #define FUN_EMP 1
 
-int  emp_pack(lista_t * L, const char *arg_output);
+int  emp_pack (lista_t * L, const char *arg_output);
 
-int emp_rpack(const char *arg_rpack, const char *arg_output);
+int emp_rpack (const char *arg_rpack, const char *arg_output);
 
 #endif
