d58df9e6ce5bdb55f983b589ae82f5c5bc96736b
elpai <rodripait@gmail.com>
2016-07-02 18:05:50 -0300

    actualizacion

--- funciones_mostrar.h
+++ funciones_mostrar.h
@@ -1,8 +1,8 @@
 #ifndef FUN_MOS
 #define FUN_MOS 1
 
-int emp_list(const char *arg_list, const char *arg_input);
+int emp_list (const char *arg_list, const char *arg_input);
 
-int emp_detail(const char *arg_det, const char *arg_input);
+int emp_detail (const char *arg_det, const char *arg_input);
 
 #endif
